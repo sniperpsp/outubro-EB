bce90cbd62b8075586fd527749e51b61ff1b7290
root <root@ip-172-0-1-211.ec2.internal>
2024-10-10 13:15:32 +0000

    envio 2

--- bia-eb-main/build.sh
+++ bia-eb-main/build.sh
@@ -1,4 +1,5 @@
 versao=$(git rev-parse HEAD | cut -c 1-7)
+echo $versao
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 730335588602.dkr.ecr.us-east-1.amazonaws.com
 docker build -t biadev .
 docker tag biadev:latest 730335588602.dkr.ecr.us-east-1.amazonaws.com/biadev:$versao
